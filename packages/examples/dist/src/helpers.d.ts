d6b9e2b47a1c96468f60381c17994d06481cf5cc
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-08-04 15:38:01 -0400

    rebuild

--- packages/examples/dist/src/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-import { ethers } from "ethers";
-import { Keypair } from "@solana/web3.js";
-export declare function getSolSigner(): Keypair;
-export declare function getEthSigner(provider: ethers.providers.Provider): ethers.Wallet;
