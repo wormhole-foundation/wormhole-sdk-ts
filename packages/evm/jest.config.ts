9eb4cf53d681c5a2d0ee67c397cf05bffb8e2092
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-08-10 22:33:16 -0400

    add jest config that works

--- packages/evm/jest.config.ts
+++ packages/evm/jest.config.ts
@@ -1,8 +1,11 @@
-module.exports = {
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageDirectory: '.coverage',
   verbose: true,
+  moduleNameMapper: {
+    '@noble/secp256k1': require.resolve('@noble/secp256k1'),
+  },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
@@ -12,3 +15,5 @@ module.exports = {
     ],
   },
 };
+
+export default jestConfig;
