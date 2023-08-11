372eb9caa58b63347e87e4ba28acd72eb25bc053
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-08-10 22:33:16 -0400

    add jest config that works

--- packages/evm/tests/unit/index.test.ts
+++ packages/evm/tests/unit/index.test.ts
@@ -2,13 +2,14 @@ import { UniversalAddress } from '@wormhole-foundation/sdk-definitions';
 import { EvmContext } from '../../src/context';
 import { ethers } from 'ethers';
 
-describe('EvmContextCreation', async () => {
+async function createContext(): Promise<EvmContext> {
   const url = 'https://rpc.ankr.com/eth';
   const provider = new ethers.JsonRpcProvider(url);
-  const ctx = await EvmContext.fromProvider(provider);
-  console.log(ctx.chain);
+  return await EvmContext.fromProvider(provider);
+}
 
-  const addy = new UniversalAddress('0x00');
-
+test('Initializes', async () => {
+  const ctx = await createContext();
+  const addy = new UniversalAddress(new Uint8Array(32));
   console.log(await ctx.transfer(addy, ['Solana', addy], 'native', 1000n));
 });
