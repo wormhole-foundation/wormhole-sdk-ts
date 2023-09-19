38a54fb24603bd815b847bfe9e50d507ade82336
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-09-19 15:36:00 -0400

    remove noble secp256k1 as dep in signature type def (#54)

--- connect/src/wormhole.ts
+++ connect/src/wormhole.ts
@@ -22,6 +22,7 @@ import {
   Contracts,
   TxHash,
   WormholeMessageId,
+  isTokenId,
 } from '@wormhole-foundation/sdk-definitions';
 import axios, { AxiosResponse } from 'axios';
 
@@ -31,7 +32,6 @@ import { CONFIG } from './constants';
 import { TokenTransfer } from './protocols/tokenTransfer';
 import { CCTPTransfer } from './protocols/cctpTransfer';
 import { TransactionStatus } from './api';
-import { isTokenId } from '@wormhole-foundation/sdk-definitions/src';
 
 export class Wormhole {
   protected _platforms: Map<PlatformName, Platform<PlatformName>>;
