cb6c92cfbb6a4c4c612fc700be47a8c93647fa1a
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-10-13 10:26:16 -0400

    rebuild docs (#89)

--- core/definitions/__tests__/bamVaa.ts
+++ core/definitions/__tests__/bamVaa.ts
@@ -1,4 +1,4 @@
-import { uint8ArrayToHexByteString } from "@wormhole-foundation/sdk-base/src/utils";
+import { uint8ArrayToHexByteString } from "@wormhole-foundation/sdk-base";
 import "../src/payloads/bam";
 import { deserialize, deserializePayload, serializePayload } from "../src/vaa";
 
@@ -13,22 +13,25 @@ const cases = [
     targetChain: "Base",
     targetAddress: baseGoerliBamAppAddress,
     senderAddress: goerliBamAppAddress,
-    contents: "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000",
-    payload: "0xbb00000000000000000000001e001444fbfee0af8efa9e580760844f6159a8e2124b53001444fbfee0af8efa9e580760844f6159a8e2124b5300600000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000",
-  }
+    contents:
+      "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000",
+    payload:
+      "0xbb00000000000000000000001e001444fbfee0af8efa9e580760844f6159a8e2124b53001444fbfee0af8efa9e580760844f6159a8e2124b5300600000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000",
+  },
 ];
-
 
 describe("BAM VAA tests", function () {
   it("should correctly deserialize a BAM VAA", function () {
     for (const testCase of cases) {
       const vaaBytes = Buffer.from(testCase.vaa, "base64");
 
-      const {payload} = deserialize(payloadLiteral, vaaBytes);
+      const { payload } = deserialize(payloadLiteral, vaaBytes);
 
       const targetAddress = uint8ArrayToHexByteString(payload.targetAddress);
       const senderAddress = uint8ArrayToHexByteString(payload.senderAddress);
-      const parsedContents = uint8ArrayToHexByteString(payload.contents as Uint8Array);
+      const parsedContents = uint8ArrayToHexByteString(
+        payload.contents as Uint8Array,
+      );
 
       expect(payload).toBeTruthy();
       expect(payload.targetChain).toEqual(testCase.targetChain);
@@ -42,11 +45,14 @@ describe("BAM VAA tests", function () {
     for (const testCase of cases) {
       const vaaBytes = Buffer.from(testCase.vaa, "base64");
 
-      const {payload} = deserialize(payloadLiteral, vaaBytes);
+      const { payload } = deserialize(payloadLiteral, vaaBytes);
 
-      const serialized = serializePayload(payloadLiteral, payload);
+      // TODO: this fn might return a payload somehow, not uint8array?
+      // @ts-ignore
+      const serialized: Uint8Array = serializePayload(payloadLiteral, payload);
+
       const serializedHex = uint8ArrayToHexByteString(serialized);
       expect(serializedHex).toEqual(testCase.payload);
     }
-  })
+  });
 });
