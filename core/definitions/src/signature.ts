d832d4a6a46f4fce00b2ec13f7c4bdd242134308
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-10-20 16:21:03 -0400

    fix issue with serializing signature (#112)

--- core/definitions/src/signature.ts
+++ core/definitions/src/signature.ts
@@ -4,12 +4,12 @@ export class Signature {
     readonly r: bigint,
     readonly s: bigint,
     readonly v: number,
-  ) {}
+  ) { }
 
   toUint8Array(): Uint8Array {
     const buff = new Uint8Array(65);
-    buff.set(Buffer.from(this.r.toString(16), "hex"));
-    buff.set(Buffer.from(this.s.toString(16), "hex"), 32);
+    buff.set(Buffer.from(this.r.toString(16).padStart(64, "0"), "hex"));
+    buff.set(Buffer.from(this.s.toString(16).padStart(64, "0"), "hex"), 32);
     buff.set([this.v], 64);
     return buff;
   }
