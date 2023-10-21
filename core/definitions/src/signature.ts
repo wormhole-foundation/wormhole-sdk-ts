ed8eb53c4a63e9511f4ae7bf350ed88e2e039dc5
Ben <ben.guidarelli@gmail.com>
2023-10-21 09:06:29 -0400

    no special serde ser

--- core/definitions/src/signature.ts
+++ core/definitions/src/signature.ts
@@ -1,20 +1,9 @@
 // Signature represents the secp256k1 signature of a Guardian
+// use layout-items/signature.ts to serialize/deserialize
 export class Signature {
   constructor(
     readonly r: bigint,
     readonly s: bigint,
     readonly v: number,
   ) { }
-
-  toUint8Array(): Uint8Array {
-    const buff = new Uint8Array(65);
-    buff.set(Buffer.from(this.r.toString(16).padStart(64, "0"), "hex"));
-    buff.set(Buffer.from(this.s.toString(16).padStart(64, "0"), "hex"), 32);
-    buff.set([this.v], 64);
-    return buff;
-  }
-
-  toBuffer(): Buffer {
-    return Buffer.from(this.toUint8Array());
-  }
 }
