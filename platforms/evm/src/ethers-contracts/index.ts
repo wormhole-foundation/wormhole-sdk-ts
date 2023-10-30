bfb22f3ca6e08b598ea55ae5abfc00418438643a
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-10-30 06:31:04 -0400

    remove more ethers contracts

--- platforms/evm/src/ethers-contracts/index.ts
+++ platforms/evm/src/ethers-contracts/index.ts
@@ -1,5 +1,2 @@
 export * from './factories';
 export * from './TokenImplementation';
-export * from './Implementation';
-export { WormholeRelayer } from './WormholeRelayer';
-export { Wormhole } from './Wormhole';
