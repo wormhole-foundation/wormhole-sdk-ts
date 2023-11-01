4a2854ca90a9ee7258ed539c109a4269cebe8577
Andreas <41449730+nonergodic@users.noreply.github.com>
2023-11-01 03:27:36 -0700

    unify circle layout specification and fix type information (#136)

--- core/definitions/src/protocols/cctp.ts
+++ core/definitions/src/protocols/cctp.ts
@@ -7,34 +7,47 @@ import {
 } from "@wormhole-foundation/sdk-base";
 import { ChainAddress } from "../address";
 import { CircleMessageId } from "../attestation";
-import { universalAddressItem } from "../layout-items";
+import {
+  universalAddressItem,
+  amountItem,
+  circleDomainItem,
+  circleNonceItem
+} from "../layout-items";
 import "../payloads/connect";
 import { RpcConnection } from "../rpc";
 import { AnyAddress, TokenId } from "../types";
 import { UnsignedTransaction } from "../unsignedTransaction";
 import { keccak256 } from "../utils";
 
+const messageVersionItem = { binary: "uint", size: 4, custom: 0, omit: true } as const;
+
 // https://developers.circle.com/stablecoin/docs/cctp-technical-reference#message
-const circleBurnMessageLayout: Layout = [
-  { name: "version", binary: "uint", size: 4 },
+const circleBurnMessageLayout = [
+  // messageBodyVersion is:
+  // * immutable: https://github.com/circlefin/evm-cctp-contracts/blob/adb2a382b09ea574f4d18d8af5b6706e8ed9b8f2/src/TokenMessenger.sol#L107
+  // * 0: https://etherscan.io/address/0xbd3fa81b58ba92a82136038b25adec7066af3155#readContract
+  { name: "messageBodyVersion", ...messageVersionItem },
   { name: "burnToken", ...universalAddressItem },
   { name: "mintRecipient", ...universalAddressItem },
-  { name: "amount", binary: "uint", size: 32 },
+  { name: "amount", ...amountItem },
   { name: "messageSender", ...universalAddressItem },
-];
+] as const satisfies Layout;
 
 // TODO: convert domain to chain name?
-export const circleMessageLayout: Layout = [
-  { name: "version", binary: "uint", size: 4 },
-  { name: "sourceDomain", binary: "uint", size: 4 },
-  { name: "destinationDomain", binary: "uint", size: 4 },
-  { name: "nonce", binary: "uint", size: 8 },
+export const circleMessageLayout = [
+  // version is:
+  // * immutable: https://github.com/circlefin/evm-cctp-contracts/blob/adb2a382b09ea574f4d18d8af5b6706e8ed9b8f2/src/MessageTransmitter.sol#L75
+  // * 0: https://etherscan.io/address/0x0a992d191deec32afe36203ad87d7d289a738f81#readContract
+  { name: "version", ...messageVersionItem },
+  { name: "sourceDomain", ...circleDomainItem },
+  { name: "destinationDomain", ...circleDomainItem },
+  { name: "nonce", ...circleNonceItem },
   { name: "sender", ...universalAddressItem },
   { name: "recipient", ...universalAddressItem },
   { name: "destinationCaller", ...universalAddressItem },
   // TODO: is this the only message body we'll get?
   { name: "payload", binary: "object", layout: circleBurnMessageLayout },
-];
+] as const satisfies Layout;
 
 export const deserializeCircleMessage = (
   data: Uint8Array,
