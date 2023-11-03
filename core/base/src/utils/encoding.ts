7c0a77953faf6f79da9d5be92d3e6c3d30ad4b8f
nonergodic <nonergodic@gmail.com>
2023-11-03 02:12:26 -0700

    tidy up encoding

--- core/base/src/utils/encoding.ts
+++ core/base/src/utils/encoding.ts
@@ -1,58 +1,43 @@
 import { base16, base64, base58 } from '@scure/base';
 
-
 export const stripPrefix = (prefix: string, str: string): string =>
-    str.startsWith(prefix) ? str.slice(prefix.length) : str;
+  str.startsWith(prefix) ? str.slice(prefix.length) : str;
 
-// This function uses a regex string to check if the input could
-// possibly be base64 encoded.
-// WARNING:  There are clear text strings that are NOT base64 encoded
-//           that will pass this check.
-export function isBase64Encoded(input: string): boolean {
-    const b64Regex = new RegExp('^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$');
-    return b64Regex.test(input);
-}
-
-
-function isHexEncoded(input: string): boolean {
-    const regex = /^[0-9a-fA-F]+$/;
-    const normalized = stripPrefix('0x', input)
-    return normalized.length % 2 === 0 && regex.test(normalized);
-}
-
+const isHexRegex = /^(?:0x)?[0-9a-fA-F]+$/;
 export const hex = {
-    valid: isHexEncoded,
-    decode: (input: string) => base16.decode(stripPrefix('0x', input).toUpperCase()),
-    encode: (input: string | Uint8Array, prefix: boolean = false) => {
-        input = typeof input === "string" ? toUint8Array(input) : input;
-        return (prefix ? "0x" : "") + base16.encode(input).toLowerCase()
-    }
+  valid: (input: string) => isHexRegex.test(input),
+  decode: (input: string) => base16.decode(stripPrefix("0x", input).toUpperCase()),
+  encode: (input: string | Uint8Array, prefix: boolean = false) => {
+    input = typeof input === "string" ? toUint8Array(input) : input;
+    return (prefix ? "0x" : "") + base16.encode(input).toLowerCase()
+  }
 }
 
+// regex string to check if the input could possibly be base64 encoded.
+// WARNING: There are clear text strings that are NOT base64 encoded that will pass this check.
+const isB64Regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
 export const b64 = {
-    valid: isBase64Encoded,
-    decode: base64.decode,
-    encode: (input: string | Uint8Array) => base64.encode(typeof input === "string" ? toUint8Array(input) : input)
+  valid: (input: string) => isB64Regex.test(input),
+  decode: base64.decode,
+  encode: (input: string | Uint8Array) =>
+    base64.encode(typeof input === "string" ? toUint8Array(input) : input)
 }
 
 export const b58 = {
-    decode: base58.decode,
-    encode: (input: string | Uint8Array) => base58.encode(typeof input === "string" ? toUint8Array(input) : input),
+  decode: base58.decode,
+  encode: (input: string | Uint8Array) =>
+    base58.encode(typeof input === "string" ? toUint8Array(input) : input),
 }
 
 export const bignum = {
-    decode: (input: string) => BigInt(input),
-    encode: (input: bigint, prefix: boolean = false) => (prefix ? "0x" : "") + input.toString(16)
+  decode: (input: string) => BigInt(input),
+  encode: (input: bigint, prefix: boolean = false) => (prefix ? "0x" : "") + input.toString(16)
 }
 
+export const toUint8Array = (value: string | bigint): Uint8Array =>
+  typeof value === "bigint"
+  ? toUint8Array(bignum.encode(value))
+  : (new TextEncoder()).encode(value);
 
-export const toUint8Array = (value: string | bigint): Uint8Array => {
-    if (typeof value === "bigint")
-        return toUint8Array(bignum.encode(value))
-
-    return (new TextEncoder()).encode(value);
-}
-
-export const fromUint8Array = (value: Uint8Array): string => {
-    return (new TextDecoder()).decode(value);
-}
+export const fromUint8Array = (value: Uint8Array): string =>
+  (new TextDecoder()).decode(value);
