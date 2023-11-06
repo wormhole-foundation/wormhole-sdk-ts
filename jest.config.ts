4da19105c692113b4dc5f26da4defe2cb24d490e
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-11-06 16:39:40 -0500

    adding typechecking tool (#143)

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   verbose: true,
-  modulePathIgnorePatterns: ["mocks", "helpers", "staging"],
+  modulePathIgnorePatterns: ["mocks", "helpers", "staging", "typechecks"],
   moduleNameMapper: {
     "@noble/secp256k1": require.resolve("@noble/secp256k1"),
   },
