d17313d3a872639f3d86db4979e15e128237b709
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-11-08 11:37:29 -0500

    adding util fn to estimate round when a tx might be final

--- core/base/__tests__/finality.ts
+++ core/base/__tests__/finality.ts
@@ -1,9 +1,42 @@
-import { finalityThreshold } from "../src/constants/finality";
+import { finalityThreshold, consistencyLevelToBlock } from "../src/constants/finality";
 
 describe("Finality tests", function () {
-  const mainnetFinality = finalityThreshold("Mainnet", "Ethereum");
-  it("should correctly access values", function () {
-    expect(mainnetFinality).toEqual(64);
-  });
+
+  test("Accesses number of rounds", () => {
+    expect(finalityThreshold("Ethereum")).toEqual(64);
+    expect(finalityThreshold("Algorand")).toEqual(0);
+    expect(finalityThreshold("Solana")).toEqual(32);
+  })
+
+  const fromBlock = 100n;
+
+  const instantLevel = 200;
+  test("Estimates rounds from instant consistency level", () => {
+    expect(consistencyLevelToBlock("Algorand", fromBlock, instantLevel)).toEqual(100n);
+    expect(consistencyLevelToBlock("Solana", fromBlock, instantLevel)).toEqual(100n);
+    expect(consistencyLevelToBlock("Terra", fromBlock, instantLevel)).toEqual(100n);
+  })
+
+  const safeLevel = 201;
+  test("Estimates rounds from safe consistency level", () => {
+    // 100 + (32 - (100 % 32))
+    expect(consistencyLevelToBlock("Ethereum", fromBlock, safeLevel)).toEqual(128n);
+    // 100 + consistency level as rounds 
+    expect(consistencyLevelToBlock("Bsc", fromBlock, safeLevel)).toEqual(301n);
+    // 100 + 0 (instant)
+    expect(consistencyLevelToBlock("Algorand", fromBlock, safeLevel)).toEqual(100n);
+  })
+
+  const finalizedLevel = 1;
+  test("Estimates rounds from finalized consistency level", () => {
+    // 100 + (# final rounds)
+    expect(consistencyLevelToBlock("Ethereum", fromBlock, finalizedLevel)).toEqual(164n);
+    expect(consistencyLevelToBlock("Solana", fromBlock, finalizedLevel)).toEqual(132n);
+    // 100 + 0 (instant)
+    expect(consistencyLevelToBlock("Algorand", fromBlock, finalizedLevel)).toEqual(100n);
+    // L2 required
+    expect(consistencyLevelToBlock("Polygon", fromBlock, finalizedLevel)).toEqual(612n);
+  })
+
 });
 
--- core/base/src/constants/finality.ts
+++ core/base/src/constants/finality.ts
@@ -2,44 +2,44 @@ import { Network } from "./networks";
 import { ChainName } from "./chains";
 import { constMap, RoArray } from "../utils";
 
-// Number of blocks before a transaction is considered final
-const shareFinalities = [
+// https://www.notion.so/Finality-in-Wormhole-78ffa423abd44b7cbe38483a16040d83
+
+// Recognized Consistency Levels for determining when a guardian 
+// may sign a VAA for a given wormhole message 
+export enum ConsistencyLevels {
+  Immediate = 200,
+  Safe = 201,
+}
+
+
+// Number of blocks before a transaction is considered "safe"
+// In this case its the number of rounds for each epoch, once an epoch
+// is completed, the transaction is considered safe
+const safeThresholds = [
+  ["Ethereum", 32], // number of rounds in an epoch
+] as const satisfies RoArray<readonly [ChainName, number]>;
+export const safeThreshold = constMap(safeThresholds);
+
+// Number of blocks before a transaction is considered "final"
+const finalityThresholds = [
   ["Ethereum", 64],
-  ["Sepolia", 64],
   ["Solana", 32],
-  ["Polygon", 64], // Note this is faster on !Mainnet currently but only because of guardian settings
+  ["Polygon", 512],
   ["Bsc", 15],
-  ["Avalanche", 1],
   ["Fantom", 1],
   ["Celo", 1],
   ["Moonbeam", 1],
+  ["Avalanche", 0],
   ["Sui", 0],
+  ["Algorand", 0],
   ["Aptos", 0],
   ["Sei", 0],
-  ["Algorand", 0],
-] as const;
-
-const finalityThresholds = [
-  [
-    "Mainnet",
-    [
-      ["Ethereum", 64],
-      ["Solana", 32],
-      ["Polygon", 512],
-      ["Bsc", 15],
-      ["Avalanche", 1],
-      ["Fantom", 1],
-      ["Celo", 1],
-      ["Moonbeam", 1],
-      ["Sui", 0],
-      ["Aptos", 0],
-      ["Sei", 0],
-    ],
-  ],
-  ["Testnet", shareFinalities],
-  ["Devnet", shareFinalities],
-] as const satisfies RoArray<readonly [Network, RoArray<readonly [ChainName, number]>]>;
-
+  ["Near", 0],
+  ["Terra", 0],
+  ["Terra2", 0],
+  ["Xpla", 0],
+  ["Injective", 0],
+] as const satisfies RoArray<readonly [ChainName, number]>;
 export const finalityThreshold = constMap(finalityThresholds);
 
 // number of milliseconds between blocks
@@ -81,20 +81,78 @@ const blockTimeMilliseconds = [
   ["Btc", 600000],
   ["Pythnet", 400],
 ] as const satisfies RoArray<readonly [ChainName, number]>;
-
 export const blockTime = constMap(blockTimeMilliseconds);
+
+// Estimate the block number that a VAA might be available
+// for a given chain, initial block where the tx was submitted
+// and consistency level 
+export function consistencyLevelToBlock(
+  chain: ChainName,
+  fromBlock: bigint,
+  consistencyLevel: number,
+): bigint {
+  // We're done
+  if (consistencyLevel === ConsistencyLevels.Immediate) return fromBlock;
+
+  // Bsc is the only chain that treats consistency level as # of blocks
+  if (chain === "Bsc") return fromBlock + BigInt(consistencyLevel);
+
+  // On Solana 0 is "confirmed", for now just return fromBlock since we 
+  // have no way of estimating when 66% of the network will have confirmed
+  if (chain === "Solana" && consistencyLevel === 0) return fromBlock;
+
+  // Get the number of blocks until finalized
+  const chainFinality = finalityThreshold.get(chain);
+  if (chainFinality === undefined) throw new Error("Cannot find chain finality for " + chain);
+
+  // If chain finality threshold is 0, it's always instant
+  if (chainFinality === 0) return fromBlock;
+
+  // We've handled all the other cases so anything != safe is `finalized`
+  if (consistencyLevel !== ConsistencyLevels.Safe)
+    return fromBlock + BigInt(chainFinality);
+
+  // Now interesting stuff
+  const safeRounds = safeThreshold.get(chain);
+  if (safeRounds === undefined) throw new Error("Cannot find safe threshold for " + chain);
+
+  switch (chain) {
+    case "Ethereum":
+      // On ethereum, "safe" is 1 epoch, return the number of blocks until the end
+      // of the current epoch
+      // 0 is end of epoch, 1 is start
+      const epochPosition = fromBlock % BigInt(safeRounds);
+      const blocksUntilEndOfEpoch =
+        epochPosition === 0n ? 0n : BigInt(safeRounds) - epochPosition;
+      return fromBlock + blocksUntilEndOfEpoch;
+
+    default:
+      throw new Error("Only Ethereum safe is supported for now");
+  }
+}
+
 
 // Some chains are required to post proof of their blocks to other chains
 // and the transaction containing that proof must be finalized
 // before a transaction contained in one of those blocks is considered final
-// const rollupContracts = [
-//   // mainnet/polygon/ethereum 0x86E4Dc95c7FBdBf52e33D563BbDB00823894C287
-//   // mainnet/optimism/ethereum 0xdfe97868233d1aa22e815a266982f2cf17685a27
-//   // mainnet/arbitrum/ethereum 0x1c479675ad559dc151f6ec7ed3fbf8cee79582b6
-//   //
-//   // testnet/polygon/ethereum 0x2890ba17efe978480615e330ecb65333b880928e
-//   // testnet/arbitrum/ethereum 0x45af9ed1d03703e480ce7d328fb684bb67da5049 // TODO, is there another one??
-//   // testnet/optimism/ethereum 0xe6dfba0953616bacab0c9a8ecb3a9bba77fc15c0
-//   //
-// ];
-//
+const rollupContractAddresses = [
+  [
+    "Mainnet",
+    [
+      ["Polygon", ["Ethereum", "0x86E4Dc95c7FBdBf52e33D563BbDB00823894C287"]],
+      ["Optimism", ["Ethereum", "0xdfe97868233d1aa22e815a266982f2cf17685a27"]],
+      ["Arbitrum", ["Ethereum", "0x1c479675ad559dc151f6ec7ed3fbf8cee79582b6"]],
+    ],
+  ],
+  [
+    "Testnet",
+    [
+      ["Polygon", ["Ethereum", "0x2890ba17efe978480615e330ecb65333b880928e"]],
+      ["Optimism", ["Ethereum", "0xe6dfba0953616bacab0c9a8ecb3a9bba77fc15c0"]],
+      ["Arbitrum", ["Ethereum", "0x45af9ed1d03703e480ce7d328fb684bb67da5049"]], // TODO double check
+    ],
+  ],
+] as const satisfies RoArray<readonly [Network, RoArray<readonly [ChainName, readonly [ChainName, string]]>]>;
+
+export const rollupContracts = constMap(rollupContractAddresses);
+
