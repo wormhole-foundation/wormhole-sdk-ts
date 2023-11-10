cebc800d412c653f9a4d05fdbb2dbc317834e3ec
SilentRhetoric <bwhippo@gmail.com>
2023-11-10 16:32:58 -0500

    Preparing to merge

---
+++ platforms/algorand/protocols/tokenBridge/src/index.ts
@@ -0,0 +1,5 @@
+import { registerProtocol } from '@wormhole-foundation/connect-sdk';
+import { AlgorandTokenBridge } from './tokenBridge';
+registerProtocol('Algorand', 'TokenBridge', AlgorandTokenBridge);
+
+export * from './tokenBridge';
--- platforms/algorand/protocols/tokenBridge/src/tokenBridge.ts
+++ platforms/algorand/protocols/tokenBridge/src/tokenBridge.ts
@@ -1,44 +1,44 @@
-import {
-  toChainId,
-  chainIdToChain,
-  Network,
-  toChainName,
-  serialize,
-  UniversalAddress,
-  ChainAddress,
-  TokenBridge,
-  TxHash,
-  keccak256,
-  TokenId,
-  NativeAddress,
-  toNative,
-  ErrNotWrapped,
-  TokenTransferTransaction,
-  RpcConnection,
-} from '@wormhole-foundation/connect-sdk';
-import { AlgorandContracts } from '../../../src/contracts';
+import { Network } from '@wormhole-foundation/connect-sdk';
+import { ChainId } from '@wormhole-foundation/connect-sdk';
+import { TokenBridge } from '@wormhole-foundation/connect-sdk';
+import { AlgorandChainName } from '../../../src';
+import { Algodv2 } from 'algosdk';
+import { Contracts } from '@wormhole-foundation/connect-sdk';
+import { toChainId } from '@wormhole-foundation/connect-sdk';
 
 export class AlgorandTokenBridge implements TokenBridge<'Algorand'> {
   readonly chainId: ChainId;
-  readonly tokenBridge: TokenBridgeContract;
-  readonly coreBridge: CoreBridgeContract;
+  readonly tokenBridgeAddress: string;
+  readonly coreAddress: string;
+  readonly tokenBridge: Program<TokenBridgeContract>;
 
   private constructor(
     readonly network: Network,
     readonly chain: AlgorandChainName,
-    readonly connection: Connection,
-    readonly contracts: AlgorandContracts,
+    readonly connection: Algodv2,
+    readonly contracts: Contracts,
   ) {
     this.chainId = toChainId(chain);
-    this.tokenBridge = this.contracts.getTokenBridge(chain, connection);
-    this.coreBridge = this.contracts.getCore(chain, connection);
-  }
 
-  static async fromProvider(
-    connection: RpcConnection<'Algorand'>,
-    contracts: AlgorandContracts,
-  ): Promise<AlgorandTokenBridge> {
-    const [network, chain] = await AlgorandPlatform.chainFromRpc(connection);
-    return new AlgorandTokenBridge(network, chain, connection, contracts);
+    const tokenBridgeAddress = contracts.tokenBridge;
+    if (!tokenBridgeAddress) {
+      throw new Error(
+        `TokenBridge contract address for chain ${chain} not found`,
+      );
+    }
+    this.tokenBridgeAddress = tokenBridgeAddress;
+
+    const coreBridgeAddress = contracts.coreBridge;
+    if (!coreBridgeAddress)
+      throw new Error(
+        `CoreBridge contract address for chain ${chain} not found`,
+      );
+
+    this.coreAddress = coreBridgeAddress;
+
+    this.tokenBridge = createReadOnlyTokenBridgeProgramInterface(
+      tokenBridgeAddress,
+      connection,
+    );
   }
 }
