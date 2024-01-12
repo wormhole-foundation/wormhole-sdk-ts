c1150fb9740ed1cb8548c1ac13972a1bcc5906be
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-01-12 12:41:49 -0500

    routes: add token bridge route (#217)

--- connect/src/index.ts
+++ connect/src/index.ts
@@ -11,6 +11,8 @@ export * as tasks from "./tasks";
 export * as circleApi from "./circle-api";
 export * as api from "./whscan-api";
 
+export * as routes from "./routes";
+
 // Re-export from core packages
 export * from "@wormhole-foundation/sdk-base";
 export * from "@wormhole-foundation/sdk-definitions";
---
+++ connect/src/routes/index.ts
@@ -0,0 +1,3 @@
+export * from "./route";
+export * from "./resolver";
+export * from "./tokenBridgeRoute";
