49d6b0d7b9216db94b3cc4d7e1a0c78206a5ada0
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-01-12 12:41:49 -0500

    routes: add token bridge route (#217)

--- connect/src/routes/route.ts
+++ connect/src/routes/route.ts
@@ -1,75 +1,64 @@
 import { Wormhole } from "../wormhole";
 
-import {
-  TokenId,
-  Signer,
-  TransactionId,
-  ChainAddress,
-} from "@wormhole-foundation/sdk-definitions";
+import { ChainAddress, Signer, TokenId, TransactionId } from "@wormhole-foundation/sdk-definitions";
 
 import { Network } from "@wormhole-foundation/sdk-base";
+import { UnknownRoute } from "./resolver";
 
-export interface TransferRequest {
+export interface RouteTransferRequest {
   from: ChainAddress;
   to: ChainAddress;
-  source: TokenId;
-  destination: TokenId;
   amount: bigint;
+  source: TokenId;
+  destination?: TokenId;
 }
 
-export type ValidationResult<T, E = Error> = { valid: T } | { valid: false, error: E };
+export type ValidationResult<T, E = Error> = { valid: T } | { valid: false; error: E };
 
-export type ValidationError = 'Amount too small' | 'Some other error';
+export type ValidationError = "Amount too small" | "Some other error";
 
-export abstract class Route<N extends Network> {
+export abstract class Route<N extends Network, OP, IR> {
   wh: Wormhole<N>;
-  request: TransferRequest;
+  request: RouteTransferRequest;
 
   abstract readonly NATIVE_GAS_DROPOFF_SUPPORTED: boolean;
 
   // true means this is a one-transaction route (using a relayer)
   abstract readonly IS_AUTOMATIC: boolean;
 
-  public constructor(wh: Wormhole<N>, request: TransferRequest) {
+  public constructor(wh: Wormhole<N>, request: RouteTransferRequest) {
     this.wh = wh;
     this.request = request;
   }
 
   public abstract isSupported(): Promise<boolean>;
 
+  public abstract validate(options: OP): Promise<ValidationResult<boolean, ValidationError>>;
+
+  public abstract initiate(sender: Signer, options: OP): Promise<IR>;
+}
+
+export abstract class AutomaticRoute<N extends Network, OP, IR> extends Route<N, OP, IR> {
+  IS_AUTOMATIC = false;
   public abstract isAvailable(): Promise<boolean>;
-
-  public abstract validate(): Promise<ValidationResult<boolean, ValidationError>>;
-
-  public abstract execute(sender: Signer, options: any): Promise<TransactionId[]>;
-
 }
 
-// --- example:
-
-interface MayanOptions {
-  gasdropoff: number;
-  slipper: number;
+export function isAutomatic<N extends Network>(
+  route: UnknownRoute<N>,
+): route is AutomaticRoute<N, unknown, unknown> {
+  return (
+    (route as AutomaticRoute<N, unknown, unknown>).isAvailable !== undefined && route.IS_AUTOMATIC
+  );
 }
 
-export class MayanSwapRoute<N extends Network> extends Route<N> {
+export abstract class ManualRoute<N extends Network, OP, IR> extends Route<N, OP, IR> {
+  NATIVE_GAS_DROPOFF_SUPPORTED = false;
+  IS_AUTOMATIC = false;
+  public abstract complete(sender: Signer, msg: IR): Promise<TransactionId[]>;
+}
 
-  NATIVE_GAS_DROPOFF_SUPPORTED = true;
-  IS_AUTOMATIC = true;
-
-  async isSupported(): Promise<boolean> {
-    return true
-  }
-
-  async isAvailable(): Promise<boolean> {
-    return true
-  }
-
-  async validate(): Promise<ValidationResult<boolean, ValidationError>> {
-    return { valid: true }
-  }
-
-  async execute(signer: Signer, options: MayanOptions): Promise<TransactionId[]> {
-    return []
-  }
+export function isCompletable<N extends Network>(
+  route: UnknownRoute<N>,
+): route is ManualRoute<N, unknown, unknown> {
+  return (route as ManualRoute<N, unknown, unknown>).complete !== undefined;
 }
