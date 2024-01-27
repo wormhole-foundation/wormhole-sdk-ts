16c511796bf0a804d482e03fb781c2f90a402434
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-01-27 10:01:46 -0500

    Provide Mocks (#286)

--- connect/src/routes/index.ts
+++ connect/src/routes/index.ts
@@ -2,7 +2,10 @@ export * from "./route";
 export * from "./resolver";
 export * from "./request";
 export * from "./types";
+export * from "./common";
 
 export * from "./tokenBridge";
 export * from "./portico";
 export * from "./cctp";
+
+export * from "./mock";
