e4196d723c176a6fee018d4b1ab0af343aca8a3c
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-01-30 07:59:42 -0500

    remove secp package from testing files

--- platforms/evm/jest.config.ts
+++ platforms/evm/jest.config.ts
@@ -1,15 +1,12 @@
-import type { JestConfigWithTsJest } from "ts-jest";
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest",
+  preset: 'ts-jest',
   verbose: true,
-  modulePathIgnorePatterns: ["mocks"],
-  moduleNameMapper: {
-    "@noble/secp256k1": require.resolve("@noble/secp256k1"),
-  },
+  modulePathIgnorePatterns: ['mocks'],
   transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
+    '^.+\\.tsx?$': [
+      'ts-jest',
       {
         isolatedModules: true,
       },
