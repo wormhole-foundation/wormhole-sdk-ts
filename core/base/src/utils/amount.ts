03445b081662c8275245c3e045300f919fdca09c
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-01-30 15:13:26 -0500

    Amount logic cleanup (#295)

--- core/base/src/utils/amount.ts
+++ core/base/src/utils/amount.ts
@@ -7,7 +7,7 @@ export interface Amount {
   amount: string;
   /* Number of decimal places in amount */
   decimals: number;
-};
+}
 
 /**
  * Parses a string or number into an Amount, given a decimal level
@@ -21,7 +21,7 @@ export function parseAmount(amount: string | number, decimals: number): Amount {
   amount = amount.toString();
 
   // TODO :)
-  if (amount.includes('e')) throw new Error('Scientific notation is not supported yet by Amount');
+  if (amount.includes("e")) throw new Error("Scientific notation is not supported yet by Amount");
 
   const chunks = amount.split(".");
   if (chunks.length > 2) throw "Too many decimals";
@@ -29,69 +29,82 @@ export function parseAmount(amount: string | number, decimals: number): Amount {
   let [whole, partial] =
     chunks.length === 0 ? ["0", ""] : chunks.length === 1 ? [chunks[0], ""] : chunks;
 
-  if (decimals < partial.length) {
-    if (partial.substring(decimals) != '0'.repeat(partial.length - decimals)) {
-      throw new Error('Amount: invalid input. Decimals too low.');
-    } else {
-      partial = partial.substring(0, decimals);
-    }
+  partial = partial.padEnd(decimals, "0");
+
+  if (partial.length > decimals) {
+    // if we'd be cutting off non-zero decimals, throw
+    if (BigInt(partial.substring(decimals)) !== 0n)
+      throw new Error("Amount: invalid input. Decimals too low.");
+
+    // otherwise, truncate the partial to exactly the number of decimals
+    partial = partial.substring(0, decimals);
   }
 
-  // Add trailing zeroes
-  while (partial.length < decimals) partial += '0';
-
-  let amountStr = whole + partial;
-
-  while (amountStr!.length > 1 && amountStr?.startsWith('0')) {
-    amountStr = amountStr.substring(1)
-  }
-
-  return { amount: amountStr, decimals }
+  const amountStr = BigInt(whole + partial).toString();
+  return { amount: amountStr, decimals };
 }
+/**
+ * A utility function to truncate an amount to some max decimal
+ *
+ * This is useful for things like the TokenBridge, where we want to truncate
+ * the amount to represent a maximum of 8 decimals
+ *
+ * @param amount the Amount to truncate
+ * @param maxDecimals the max number of decimals we want to keep, zeroing out the rest
+ * @returns the truncated Amount
+ */
 
 export function truncateAmount(amount: Amount, maxDecimals: number): Amount {
-  const len = amount.amount.length;
-  if (len > maxDecimals) {
-    const decimalsDelta = amount.decimals - maxDecimals;
-    const truncated = amount.amount.substring(0, decimalsDelta);
-    return {
-      amount: truncated + '0'.repeat(decimalsDelta),
-      decimals: amount.decimals,
-    }
-  } else {
-    return amount;
-  }
+  if (amount.decimals <= maxDecimals) return amount;
+  const delta = BigInt(amount.decimals - maxDecimals);
+  // first scale down to maxDecimals, this will truncate the amount
+  const scaledAmount = baseUnits(amount) / 10n ** delta;
+  // then scale back to original decimals
+  const amt = scaledAmount * 10n ** delta;
+
+  return {
+    amount: amt.toString(),
+    decimals: amount.decimals,
+  };
 }
 
+/**
+ * Utility function to scale some amount to a given number of decimals
+ *
+ * This is useful for things like the TokenBridge, where we want to scale
+ * the amount from some the over-the-wire 8 decimals
+ *
+ * @param amount the amount to scale
+ * @param toDecimals the number of decimals to scale to
+ * @returns the scaled amount
+ */
 export function scaleAmount(amount: Amount, toDecimals: number): Amount {
-  const decimalsDelta = toDecimals - amount.decimals;
+  if (amount.decimals === toDecimals) return amount;
+  if (amount.amount === "0") return { amount: amount.amount, decimals: toDecimals };
 
-  if (amount.amount === '0') {
-    return { amount: amount.amount, decimals: toDecimals };
-  }
-
-  if (decimalsDelta === 0) {
-    // Nothing to do
-    return amount;
-  } if (decimalsDelta > 0) {
-    // Scaling up is easy; simply add zeroes to the base units value
+  const delta = toDecimals - amount.decimals;
+  // Scaling up is easy; simply add zeroes to the base units value
+  if (delta > 0)
     return {
-      amount: amount.amount + '0'.repeat(decimalsDelta), decimals: toDecimals
-    }
-  } else {
-    // Scaling down is trickier; we have to make sure we're not altering the amount. This should be done
-    // explicitly using truncateAmount to avoid bugs.
-    if (amount.amount.substring(amount.amount.length + decimalsDelta) === '0'.repeat(-decimalsDelta)) {
-      return {
-        amount: amount.amount.substring(0, amount.amount.length  + decimalsDelta),
-        decimals: toDecimals,
-      }
-    } else {
-      throw new Error(`scaleAmount(${JSON.stringify(amount)}, ${toDecimals}) would result in altered amount. Use truncateAmount first if you intended to truncate it.`);
-    }
-  }
-}
+      amount: amount.amount + "0".repeat(delta),
+      decimals: toDecimals,
+    };
 
+  // Scaling down is trickier; we have to make sure we're not altering the amount.
+  // This should be done explicitly using truncateAmount to avoid bugs.
+  if (BigInt(amount.amount.substring(amount.amount.length + delta)) === 0n) {
+    return {
+      amount: amount.amount.substring(0, amount.amount.length + delta),
+      decimals: toDecimals,
+    };
+  }
+
+  throw new Error(
+    `scaleAmount(${JSON.stringify(
+      amount,
+    )}, ${toDecimals}) would result in altered amount. Use truncateAmount first if you intended to truncate it.`,
+  );
+}
 
 /**
  * Directly creates an Amount given the base units and decimal level
@@ -100,7 +113,7 @@ export function scaleAmount(amount: Amount, toDecimals: number): Amount {
  * @returns An Amount, expressed as base units and decimals
  */
 export function amountFromBaseUnits(amount: bigint, decimals: number): Amount {
-  return { amount: amount.toString(), decimals }
+  return { amount: amount.toString(), decimals };
 }
 
 /**
@@ -110,7 +123,7 @@ export function amountFromBaseUnits(amount: bigint, decimals: number): Amount {
  */
 export function baseUnits(amount: Amount): bigint {
   validateAmount(amount);
-  return BigInt(amount.amount)
+  return BigInt(amount.amount);
 }
 
 /**
@@ -122,58 +135,45 @@ export function baseUnits(amount: Amount): bigint {
 export function displayAmount(amount: Amount, precision?: number): string {
   validateAmount(amount);
 
-  let whole = amount.amount.substring(0, amount.amount.length - amount.decimals);
-  let partial = amount.amount.substring(amount.amount.length - amount.decimals);
+  let whole = amount.amount.substring(0, amount.amount.length - amount.decimals).padStart(1, "0");
+  let partial = amount.amount
+    .substring(amount.amount.length - amount.decimals)
+    .padStart(amount.decimals, "0");
 
-  while (partial.length < amount.decimals) {
-    partial = '0' + partial;
-  }
-
-  if (typeof precision === 'number') {
-    while (precision < partial.length) {
-      if (partial[partial.length-1] === '0') {
-        partial = partial.substring(0, partial.length-1);
-      } else {
-        break;
-      }
+  if (precision !== undefined) {
+    while (partial.length > precision) {
+      if (partial[partial.length - 1] !== "0") break;
+      partial = partial.substring(0, partial.length - 1);
     }
-    while (precision > partial.length) {
-      partial += '0';
-    }
+    partial = partial.padEnd(precision, "0");
   }
 
-  if (whole.length === 0) whole = '0';
-
-  if (partial.length > 0) {
-    return `${whole}.${partial}`;
-  } else {
-    return whole;
-  }
+  return partial.length > 0 ? `${whole}.${partial}` : whole;
 }
 
 function validateAmountInput(amount: number | string, decimals: number): void {
-  if (typeof amount === 'number') {
+  if (typeof amount === "number") {
     if (!isFinite(amount)) throw new Error("Amount: invalid input. Amount must be finite");
-    if (amount < 0) throw new Error('Amount: invalid input. Amount cannot be negative');
+    if (amount < 0) throw new Error("Amount: invalid input. Amount cannot be negative");
   } else {
     if (!/^[0-9\.]*$/.test(amount)) {
-      throw new Error('Amount: invalid input. Must only contain digits.');
+      throw new Error("Amount: invalid input. Must only contain digits.");
     }
   }
 
   if (!isFinite(decimals)) {
-    throw new Error('Amount: invalid input. Decimals must be finite');
+    throw new Error("Amount: invalid input. Decimals must be finite");
   }
 }
 
 function validateAmount(amount: Amount): void {
   if (!/^[0-9]*$/.test(amount.amount)) {
-    throw new Error('Amount: invalid input. Must only contain digits.');
+    throw new Error("Amount: invalid input. Must only contain digits.");
   }
   if (amount.decimals < 0) {
-    throw new Error('Amount: invalid input. Decimals must be >= 0');
+    throw new Error("Amount: invalid input. Decimals must be >= 0");
   }
   if (!isFinite(amount.decimals)) {
-    throw new Error('Amount: invalid input. Decimals must be a finite number.');
+    throw new Error("Amount: invalid input. Decimals must be a finite number.");
   }
 }
