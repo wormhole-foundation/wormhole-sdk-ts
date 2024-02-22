406b003e3340ebb07fa596ad08e66a3c23648f3e
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-02-22 10:51:17 -0500

    add test, fix replace of chars

---
+++ platforms/solana/__tests__/unit/utils.test.ts
@@ -0,0 +1,27 @@
+import { camelCase, upperFirst } from '../../src';
+
+const camelCaseCases: [string, string][] = [
+  ['foo', 'foo'],
+  ['foo bar', 'fooBar'],
+  ['foo barBaz', 'fooBarBaz'],
+  ['foo bar baz', 'fooBarBaz'],
+  ['foo-bar-baz', 'fooBarBaz'],
+];
+
+const upperFirstCases: [string, string][] = [
+  ['foo', 'Foo'],
+  ['fooBar', 'FooBar'],
+  ['fooBarBaz', 'FooBarBaz'],
+];
+describe('Solana Utils tests', () => {
+  test('Test camelCase function', () => {
+    for (const [input, expected] of camelCaseCases) {
+      expect(camelCase(input)).toBe(expected);
+    }
+  });
+  test('Test upperFirst function', () => {
+    for (const [input, expected] of upperFirstCases) {
+      expect(upperFirst(input)).toBe(expected);
+    }
+  });
+});
