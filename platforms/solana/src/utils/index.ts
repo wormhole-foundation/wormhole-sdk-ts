3bf689b85f9e958634942229a4eb2ad4cfb039c4
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-02-22 11:01:34 -0500

    Remove lodash as dependency (#345)
    * removing lodash dep
    
    * add test, fix replace of chars

--- platforms/solana/src/utils/index.ts
+++ platforms/solana/src/utils/index.ts
@@ -1,2 +1,18 @@
 export * as utils from './utils';
 export * as anchor from './anchor';
+
+// camel case a string (from https://stackoverflow.com/a/2970667)
+export function camelCase(str: string): string {
+  return (
+    str
+      .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index === 0 ? word.toLowerCase() : word.toUpperCase();
+      })
+      // replace any spaces, hyphens, or underscores with an empty string
+      .replace(/[\s\-_]+/g, '')
+  );
+}
+
+export function upperFirst(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
