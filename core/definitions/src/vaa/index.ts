f6820cdcc7e4187af9b9056a102e57272c75445a
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-03-12 14:01:43 -0400

    prettier

--- core/definitions/src/vaa/index.ts
+++ core/definitions/src/vaa/index.ts
@@ -1,9 +1,17 @@
-export type { PayloadLiteral, NamedPayloads, RegisterPayloadTypes } from './registration';
+export type { PayloadLiteral, NamedPayloads, RegisterPayloadTypes } from "./registration";
 export { registerPayloadType, registerPayloadTypes } from "./registration";
 
 export type { VAA, ProtocolVAA, Payload, ProtocolPayload } from "./vaa";
 
-export type { PayloadDiscriminator } from './functions';
-export { getPayloadLayout, serialize, serializePayload, payloadDiscriminator, deserialize, deserializePayload, blindDeserializePayload } from "./functions";
+export type { PayloadDiscriminator } from "./functions";
+export {
+  getPayloadLayout,
+  serialize,
+  serializePayload,
+  payloadDiscriminator,
+  deserialize,
+  deserializePayload,
+  blindDeserializePayload,
+} from "./functions";
 
 export { createVAA } from "./create";
--- platforms/solana/protocols/cctp/src/index.ts
+++ platforms/solana/protocols/cctp/src/index.ts
@@ -4,13 +4,10 @@ import { _platform } from '@wormhole-foundation/sdk-solana';
 
 registerProtocol(_platform, 'CircleBridge', SolanaCircleBridge);
 
-import type { TokenMessenger} from './anchor-idl/tokenMessenger';
+import type { TokenMessenger } from './anchor-idl/tokenMessenger';
 import { TokenMessengerIdl } from './anchor-idl/tokenMessenger';
-import type {
-  MessageTransmitter} from './anchor-idl/messageTransmitter';
-import {
-  MessageTransmitterIdl,
-} from './anchor-idl/messageTransmitter';
+import type { MessageTransmitter } from './anchor-idl/messageTransmitter';
+import { MessageTransmitterIdl } from './anchor-idl/messageTransmitter';
 
 export const idl = {
   TokenMessengerIdl,
