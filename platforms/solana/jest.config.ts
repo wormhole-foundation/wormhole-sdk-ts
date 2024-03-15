16168d54db941611f36701c1b716f61cd866a31e
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-03-15 14:04:13 -0400

    wat

--- platforms/solana/jest.config.ts
+++ platforms/solana/jest.config.ts
@@ -3,7 +3,6 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   verbose: true,
   modulePathIgnorePatterns: ['mocks'],
-
   preset: 'ts-jest/presets/default-esm', // or other ESM presets
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
