add0def1796321653d61c05bd2024777ea49c94e
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-03-25 09:23:19 -0400

    Remove idl from types file, we're using the json one

--- platforms/solana/protocols/ntt/src/anchor-idl/wormhole_governance.ts
+++ platforms/solana/protocols/ntt/src/anchor-idl/wormhole_governance.ts
@@ -1,137 +1,68 @@
 export type WormholeGovernance = {
-  "version": "0.1.0",
-  "name": "wormhole_governance",
-  "instructions": [
+  version: '0.1.0';
+  name: 'wormhole_governance';
+  instructions: [
     {
-      "name": "governance",
-      "accounts": [
+      name: 'governance';
+      accounts: [
         {
-          "name": "payer",
-          "isMut": true,
-          "isSigner": true
+          name: 'payer';
+          isMut: true;
+          isSigner: true;
         },
         {
-          "name": "governance",
-          "isMut": true,
-          "isSigner": false
+          name: 'governance';
+          isMut: true;
+          isSigner: false;
         },
         {
-          "name": "vaa",
-          "isMut": false,
-          "isSigner": false
+          name: 'vaa';
+          isMut: false;
+          isSigner: false;
         },
         {
-          "name": "program",
-          "isMut": false,
-          "isSigner": false
+          name: 'program';
+          isMut: false;
+          isSigner: false;
         },
         {
-          "name": "replay",
-          "isMut": true,
-          "isSigner": false
+          name: 'replay';
+          isMut: true;
+          isSigner: false;
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": []
-    }
-  ],
-  "accounts": [
+          name: 'systemProgram';
+          isMut: false;
+          isSigner: false;
+        },
+      ];
+      args: [];
+    },
+  ];
+  accounts: [
     {
-      "name": "replayProtection",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: 'replayProtection';
+      type: {
+        kind: 'struct';
+        fields: [
           {
-            "name": "bump",
-            "type": "u8"
-          }
-        ]
-      }
-    }
-  ],
-  "errors": [
+            name: 'bump';
+            type: 'u8';
+          },
+        ];
+      };
+    },
+  ];
+  errors: [
     {
-      "code": 6000,
-      "name": "InvalidGovernanceChain",
-      "msg": "InvalidGovernanceChain"
+      code: 6000;
+      name: 'InvalidGovernanceChain';
+      msg: 'InvalidGovernanceChain';
     },
     {
-      "code": 6001,
-      "name": "InvalidGovernanceEmitter",
-      "msg": "InvalidGovernanceEmitter"
-    }
-  ]
-};
-
-export const IDL: WormholeGovernance = {
-  "version": "0.1.0",
-  "name": "wormhole_governance",
-  "instructions": [
-    {
-      "name": "governance",
-      "accounts": [
-        {
-          "name": "payer",
-          "isMut": true,
-          "isSigner": true
-        },
-        {
-          "name": "governance",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "vaa",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "program",
-          "isMut": false,
-          "isSigner": false
-        },
-        {
-          "name": "replay",
-          "isMut": true,
-          "isSigner": false
-        },
-        {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": []
-    }
-  ],
-  "accounts": [
-    {
-      "name": "replayProtection",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "bump",
-            "type": "u8"
-          }
-        ]
-      }
-    }
-  ],
-  "errors": [
-    {
-      "code": 6000,
-      "name": "InvalidGovernanceChain",
-      "msg": "InvalidGovernanceChain"
+      code: 6001;
+      name: 'InvalidGovernanceEmitter';
+      msg: 'InvalidGovernanceEmitter';
     },
-    {
-      "code": 6001,
-      "name": "InvalidGovernanceEmitter",
-      "msg": "InvalidGovernanceEmitter"
-    }
-  ]
+  ];
 };
