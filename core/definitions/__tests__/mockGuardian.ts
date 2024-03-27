fd5f70dc1b1b203212fb82e69003351b84b4424d
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-03-27 10:29:38 -0400

    Add mock guardian and emitter

---
+++ core/definitions/__tests__/mockGuardian.ts
@@ -0,0 +1,48 @@
+import { encoding } from "@wormhole-foundation/sdk-base";
+import { VAA, keccak256 } from "../src/index.js";
+import { MockEmitter, MockGuardians, ethValidateSig } from "../src/testing/mocks/guardian.js";
+import { makeUniversalAddress } from "../src/testing/utils/index.js";
+
+const GUARDIAN_KEY = "cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0";
+const TEST_CHAIN = "Ethereum";
+
+describe("Mock Emitter Tests", function () {});
+
+describe("Mock Guardian Tests", function () {
+  const address = makeUniversalAddress(TEST_CHAIN);
+  let gset: MockGuardians;
+  let emitter: MockEmitter;
+
+  it("Should create a Guardian set", function () {
+    gset = new MockGuardians(0, [GUARDIAN_KEY]);
+    expect(gset.getPublicKeys()).toHaveLength(1);
+  });
+
+  it("Should create an Emitter", function () {
+    emitter = new MockEmitter(address, TEST_CHAIN);
+    expect(emitter.chain).toEqual(TEST_CHAIN);
+  });
+
+  let msg: Uint8Array;
+  it("Should publish a message", function () {
+    const payload = encoding.bytes.encode("lol");
+    msg = emitter.publishMessage(0, payload, 200);
+    expect(msg).toHaveLength(57 + payload.length);
+  });
+
+  let signedVaa: VAA<"Uint8Array">;
+  it("Should sign the message published", function () {
+    signedVaa = gset.addSignatures(msg, [0]);
+  });
+
+  it("Should validate the VAA its just signed", function () {
+    // rehash the hash since we use double hash for sign
+    const hash = keccak256(signedVaa.hash);
+    const pubKeys = gset.getPublicKeys();
+    for (const sig of signedVaa.signatures) {
+      const pubkey = pubKeys[sig.guardianIndex]!;
+      const valid = ethValidateSig(sig.signature, pubkey, hash);
+      expect(valid).toBeTruthy();
+    }
+  });
+});
--- core/definitions/src/utils.ts
+++ core/definitions/src/utils.ts
@@ -1,6 +1,8 @@
-//@noble is what ethers uses under the hood
 import { sha256 } from "@noble/hashes/sha256";
 import { keccak_256, sha3_256 } from "@noble/hashes/sha3";
 import { sha512_256 } from "@noble/hashes/sha512";
 
-export { keccak_256 as keccak256, sha3_256, sha256, sha512_256 };
+import { secp256k1 } from "@noble/curves/secp256k1";
+import { ed25519 } from "@noble/curves/ed25519";
+
+export { keccak_256 as keccak256, sha3_256, sha256, sha512_256, secp256k1, ed25519 };
