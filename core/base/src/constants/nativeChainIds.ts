030087c3240d80d0b3bcb57c43f65cee561ab658
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-04-04 12:45:14 -0400

    Types: rework how native chain ids are stored and types resolved

--- core/base/src/constants/nativeChainIds.ts
+++ core/base/src/constants/nativeChainIds.ts
@@ -6,214 +6,140 @@ import type { Platform, PlatformToChains } from "./platforms.js";
 import { chainToPlatform } from "./platforms.js";
 
 // prettier-ignore
-const chainNetworkNativeChainIdEntries = [[
-  "Aptos", [
-    ["Mainnet", 1n],
-    ["Testnet", 2n],
-    ["Devnet",  0n],
-  ]], [
-  "Algorand", [
-    ["Mainnet", "mainnet-v1.0"],
-    ["Testnet", "testnet-v1.0"],
-    ["Devnet",  "sandnet-v1.0"],
-  ]], [
-  "Near", [
-    ["Mainnet", "mainnet"],
-    ["Testnet", "testnet"],
-  ]], [
-  "Cosmoshub", [
-    ["Mainnet", "cosmoshub-4"],
-    ["Testnet", "theta-testnet-001"],
-  ]], [
-  "Evmos", [
-    ["Mainnet", "evmos_9001-2"],
-    ["Testnet", "evmos_9000-4"],
-    ["Devnet",  "evmos_devnet_fake"],
-  ]], [
-  "Injective", [
-    ["Mainnet", "injective-1"],
-    ["Testnet", "injective-888"],
-    ["Devnet",  "injective_devnet_fake"],
-  ]], [
-  "Osmosis", [
-    ["Mainnet", "osmosis-1"],
-    ["Testnet", "osmo-test-5"],
-  ]], [
-  "Sei", [
-    ["Mainnet", "pacific-1"],
-    ["Testnet", "atlantic-2"],
-  ]], [
-  "Terra", [
-    ["Mainnet", "columbus-5"],
-    ["Testnet", "bombay-12"],
-  ]], [
-  "Terra2", [
-    ["Mainnet", "phoenix-1"],
-    ["Testnet", "pisco-1"],
-  ]], [
-  "Wormchain", [
-    ["Mainnet", "wormchain"],
-    ["Testnet", "wormchain-testnet-0"],
-  ]], [
-  "Xpla", [
-    ["Mainnet", "dimension_37-1"],
-    ["Testnet", "cube_47-5"],
-  ]], [
-  "Kujira", [
-    ["Mainnet", "kaiyo-1"],
-    ["Testnet", "harpoon-4"],
-  ]], [
-  "Solana", [
-    ["Mainnet", "5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d"],
-    ["Testnet", "EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG"],
-    ["Devnet",  "8wF6jKV3cKwyaVkWcoV9KpDqmkjvEYno9hKZrKx8TbZn"],
-  ]], [
-  "Sui", [
-    ["Mainnet", "35834a8a"],
-    ["Testnet", "4c78adac"],
-  ]], [
-  "Acala", [
-    ["Mainnet", 787n],
-    ["Testnet", 597n],
-  ]], [
-  "Arbitrum", [
-    ["Mainnet", 42161n], //arbitrum goerli
-    ["Testnet", 421613n],
-  ]], [
-  "Aurora", [
-    ["Mainnet", 1313161554n],
-    ["Testnet", 1313161555n],
-  ]], [
-  "Avalanche", [
-    ["Mainnet", 43114n],
-    ["Testnet", 43113n], //fuji
-  ]], [
-  "Base", [
-    ["Mainnet", 8453n],
-    ["Testnet", 84531n],
-  ]], [
-  "Bsc", [
-    ["Mainnet", 56n],
-    ["Testnet", 97n],
-    ["Devnet",  1397n]
-  ]], [
-  "Celo", [
-    ["Mainnet", 42220n],
-    ["Testnet", 44787n], //alfajores
-  ]], [
-  "Ethereum", [
-    ["Mainnet", 1n],
-    ["Testnet", 5n], //goerli
-    ["Devnet",  1337n]
-  ]], [
-  "Fantom", [
-    ["Mainnet", 250n],
-    ["Testnet", 4002n],
-  ]], [
-  "Gnosis", [
-    ["Mainnet", 100n],
-    ["Testnet", 10200n],
-  ]], [
-  "Karura", [
-    ["Mainnet", 686n],
-    ["Testnet", 596n],
-  ]], [
-  "Klaytn", [
-    ["Mainnet", 8217n],
-    ["Testnet", 1001n], //baobab
-  ]], [
-  "Moonbeam", [
-    ["Mainnet", 1284n],
-    ["Testnet", 1287n], //moonbase alpha
-  ]], [
-  "Neon", [
-    ["Mainnet", 245022934n],
-    ["Testnet", 245022940n],
-  ]], [
-  "Oasis", [
-    ["Mainnet", 42262n],
-    ["Testnet", 42261n],
-  ]], [
-  "Optimism", [
-    ["Mainnet", 10n],
-    ["Testnet", 420n],
-  ]], [
-  "Polygon", [
-    ["Mainnet", 137n],
-    ["Testnet", 80001n], //mumbai
-  ]], [
-  "Rootstock", [
-    ["Mainnet", 30n],
-    ["Testnet", 31n],
-  ]], [
-  "Sepolia", [
-    ["Testnet", 11155111n]
-  ]], [
-  "ArbitrumSepolia", [
-    ["Testnet", 421614n]
-  ]], [
-  "BaseSepolia", [
-    ["Testnet", 84532n]
-  ]], [
-  "OptimismSepolia", [
-    ["Testnet", 11155420n]
-  ]], [
-  "Holesky", [
-    ["Testnet", 17000n]
-  ]],[
-  "Neutron", [
-    ["Mainnet", "neutron-1"],
-    ["Testnet", "pion-1"],
-  ]],[
-  "Stargaze", [
-    ["Mainnet", "stargaze-1"]
-  ]],[
-  "Celestia", [
-    ["Mainnet", "celestia"],
-    ["Testnet", "mocha-4"]
-  ]],[
-  "Dymension", [
-    ["Mainnet", "dymension_1100-1"]
-  ]],[
-  "Seda",[
-    ["Testnet", "seda-1-testnet"],
-  ]],[
-  "PolygonSepolia", [
-    ["Testnet", 80002n]
-  ]],[
-  "Mantle", [
-    ["Mainnet", 5000n],
-    ["Testnet", 5003n] // Sepolia testnet
-  ]],
+const chainNetworkNativeChainIdEntries = [
   [
-  "Scroll", [
-    ["Mainnet", 534352n],
-    ["Testnet", 534351n],
-  ]],
-] as const satisfies MapLevels<[Chain, Network, bigint | string]>;
+    "Mainnet",
+    [
+      ["Aptos",     1n],
+      ["Algorand",  "mainnet-v1.0"],
+      ["Near",      "mainnet"],
+      ["Cosmoshub", "cosmoshub-4"],
+      ["Evmos",     "evmos_9001-2"],
+      ["Injective", "injective-1"],
+      ["Osmosis",   "osmosis-1"],
+      ["Sei",       "pacific-1"],
+      ["Terra",     "columbus-5"],
+      ["Terra2",    "phoenix-1"],
+      ["Wormchain", "wormchain"],
+      ["Xpla",      "dimension_37-1"],
+      ["Kujira",    "kaiyo-1"],
+      ["Solana",    "5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d"],
+      ["Sui",       "35834a8a"],
+      ["Acala",     787n],
+      ["Arbitrum",  42161n],
+      ["Aurora",    1313161554n],
+      ["Avalanche", 43114n],
+      ["Base",      8453n],
+      ["Bsc",       56n],
+      ["Celo",      42220n],
+      ["Ethereum",  1n],
+      ["Fantom",    250n],
+      ["Gnosis",    100n],
+      ["Karura",    686n],
+      ["Klaytn",    8217n],
+      ["Moonbeam",  1284n],
+      ["Neon",      245022934n],
+      ["Oasis",     42262n],
+      ["Optimism",  10n],
+      ["Polygon",   137n],
+      ["Rootstock", 30n],
+      ["Neutron",   "neutron-1"],
+      ["Stargaze",  "stargaze-1"],
+      ["Celestia",  "celestia"],
+      ["Dymension", "dymension_1100-1"],
+      ["Mantle",    5000n],
+      ["Scroll",    534352n],
+    ],
+  ],
+  [
+    "Testnet",
+    [
+      ["Aptos",           2n],
+      ["Algorand",        "testnet-v1.0"],
+      ["Near",            "testnet"],
+      ["Cosmoshub",       "theta-testnet-001"],
+      ["Evmos",           "evmos_9000-4"],
+      ["Injective",       "injective-888"],
+      ["Osmosis",         "osmo-test-5"],
+      ["Sei",             "atlantic-2"],
+      ["Terra",           "bombay-12"],
+      ["Terra2",          "pisco-1"],
+      ["Wormchain",       "wormchain-testnet-0"],
+      ["Xpla",            "cube_47-5"],
+      ["Kujira",          "harpoon-4"],
+      ["Solana",          "EtWTRABZaYq6iMfeYKouRu166VU2xqa1wcaWoxPkrZBG"],
+      ["Sui",             "4c78adac"],
+      ["Acala",           597n],
+      ["Arbitrum",        421613n],
+      ["Aurora",          1313161555n],
+      ["Avalanche",       43113n], //fuji
+      ["Base",            84531n],
+      ["Bsc",             97n],
+      ["Celo",            44787n], //alfajores
+      ["Ethereum",        5n], //goerli
+      ["Fantom",          4002n],
+      ["Gnosis",          10200n],
+      ["Karura",          596n],
+      ["Klaytn",          1001n], //baobab
+      ["Moonbeam",        1287n], //moonbase alpha
+      ["Neon",            245022940n],
+      ["Oasis",           42261n],
+      ["Optimism",        420n],
+      ["Polygon",         80001n], //mumbai
+      ["Rootstock",       31n],
+      ["Sepolia",         11155111n],
+      ["ArbitrumSepolia", 421614n],
+      ["BaseSepolia",     84532n],
+      ["OptimismSepolia", 11155420n],
+      ["PolygonSepolia",  80002n],
+      ["Holesky",         17000n],
+      ["Neutron",         "pion-1"],
+      ["Celestia",        "mocha-4"],
+      ["Seda",            "seda-1-testnet"],
+      ["Mantle",          5003n], // Sepolia testnet
+      ["Scroll",          534351n],
+    ],
+  ],
+  [
+    "Devnet",
+    [
+      ["Aptos",     0n],
+      ["Algorand", "sandnet-v1.0"],
+      ["Bsc",       1397n],
+      ["Ethereum",  1337n],
+      ["Injective","injective_devnet_fake"],
+      ["Solana",    "8wF6jKV3cKwyaVkWcoV9KpDqmkjvEYno9hKZrKx8TbZn"],
+    ],
+  ],
+] as const satisfies MapLevels<[Network, Chain, bigint | string]>;
 
-// @ts-ignore -- type instantiation too large and possibly infinite??
-export const networkChainToNativeChainId = constMap(chainNetworkNativeChainIdEntries, [[1, 0], 2]);
+export const networkChainToNativeChainId = constMap(chainNetworkNativeChainIdEntries);
+
+type NetworkChainToNativeChainId = ToMapping<typeof chainNetworkNativeChainIdEntries>;
+export type PlatformToNativeChainIds<
+  P extends Platform,
+  N extends Network,
+> = PlatformToChains<P> extends infer C
+  ? C extends keyof NetworkChainToNativeChainId[N]
+    ? NetworkChainToNativeChainId[N][C]
+    : // If the platform doesn't support the network, return the widened type for
+    // mainnet
+    C extends keyof NetworkChainToNativeChainId["Mainnet"]
+    ? Widen<NetworkChainToNativeChainId["Mainnet"][C]>
+    : never
+  : never;
 
 //When mapping a Platform and native chain id to a network and chain pair, we assume that the
 //  mapping is injective, i.e. that for any given platform there is only a single chain that
 //  has the given chain id.
 //Currently this is the case, in fact only Evm and Aptos share a chain id (1n).
-const nativeChainIdToNetworkChain = constMap(chainNetworkNativeChainIdEntries, [2, [1, 0]]);
-
-type NetworkChainToNativeChainId = ToMapping<typeof chainNetworkNativeChainIdEntries>;
-export type PlatformToNativeChainIds<P extends Platform> = PlatformToChains<P> extends infer C
-  ? C extends keyof NetworkChainToNativeChainId
-    ? NetworkChainToNativeChainId[C][keyof NetworkChainToNativeChainId[C]]
-    : never
-  : never;
-
+const nativeChainIdToNetworkChain = constMap(chainNetworkNativeChainIdEntries, [2, [0, 1]]);
 export type PlatformNativeChainIdToNetworkChainPair<
   P extends Platform,
-  CI extends PlatformToNativeChainIds<P>,
+  N extends Network = Network,
 > = PlatformToChains<P> extends infer C
-  ? ReturnType<typeof nativeChainIdToNetworkChain<CI>>[number] extends infer NCP
-    ? NCP extends readonly [Network, C]
+  ? ReturnType<typeof nativeChainIdToNetworkChain>[number] extends infer NCP
+    ? NCP extends readonly [N, C]
       ? NCP
       : never
     : never
@@ -221,12 +147,11 @@ export type PlatformNativeChainIdToNetworkChainPair<
 
 export function platformNativeChainIdToNetworkChain<
   P extends Platform,
-  CI extends PlatformToNativeChainIds<P>,
->(platform: P, chainId: Widen<CI>): PlatformNativeChainIdToNetworkChainPair<P, CI> {
+  CI extends PlatformToNativeChainIds<P, Network>,
+>(platform: P, chainId: Widen<CI>): PlatformNativeChainIdToNetworkChainPair<P> {
   //typescript really struggles to comprehend the types here so we have to help it out
-  const candidates = nativeChainIdToNetworkChain(
-    chainId as PlatformToNativeChainIds<P>,
-  ) as readonly (readonly [Network, Chain])[];
+  //@ts-ignore
+  const candidates = nativeChainIdToNetworkChain(chainId) as readonly (readonly [Network, Chain])[];
   const mustBeSingleton = candidates.filter(([_, chain]) => chainToPlatform(chain) === platform);
   if (mustBeSingleton.length !== 1)
     throw new Error(`Platform ${platform} has multiple chains with native chain id ${chainId}`);
