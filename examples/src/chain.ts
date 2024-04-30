05162d64a7be726aa561aa8d0269d1c4a32f8998
Ben Guidarelli <ben.guidarelli@gmail.com>
2024-04-30 16:27:57 -0400

    add example of constructing protocol without the use of wormhole object

---
+++ examples/src/chain.ts
@@ -0,0 +1,12 @@
+import "@wormhole-foundation/sdk-evm-core";
+import evm from "@wormhole-foundation/sdk/platforms/evm";
+
+(async function () {
+  const platform = new evm.Platform("Testnet");
+  const chain = platform.getChain("Sepolia");
+  const core = await chain.getWormholeCore();
+  const [msg] = await core.parseMessages(
+    "0xb13ae6de4aadbc33b031c572acd724e9793e2def7437c9dba9e4b7bb4f544258",
+  );
+  console.log(msg);
+})();
