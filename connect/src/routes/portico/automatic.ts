1667d65f1a98c3f92ca45266b543d8d5f092d5c9
Kevin Peters <kevin@wormholelabs.xyz>
2024-05-15 16:03:19 -0500

    Added native USDT tokens

--- connect/src/routes/portico/automatic.ts
+++ connect/src/routes/portico/automatic.ts
@@ -77,8 +77,7 @@ export class AutomaticPorticoRoute<N extends Network>
     name: "AutomaticPortico",
   };
 
-  // TODO: "ETH"?
-  private static _supportedTokens = ["WETH", "wstETH", "USDT"];
+  private static _supportedTokens = ["ETH", "WETH", "wstETH", "USDT"];
 
   static supportedNetworks(): Network[] {
     return ["Mainnet"];
@@ -100,7 +99,7 @@ export class AutomaticPorticoRoute<N extends Network>
       .flat()
       .filter((td) => {
         // Only tokens native to the chain are supported
-        return td.chain === chain;
+        return td.chain === chain && !td.original;
       });
 
     return supported.map((td) => Wormhole.tokenId(chain, td.address));
