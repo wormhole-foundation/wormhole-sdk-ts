e2e50e876eaf9a0b7482a913a37dfa05ebdd5b13
Kevin Peters <kevin@w7.xyz>
2024-07-23 12:32:42 -0500

    Sui: fix unit tests

--- platforms/sui/__tests__/unit/platform.test.ts
+++ platforms/sui/__tests__/unit/platform.test.ts
@@ -1,26 +1,29 @@
 import { expect, test } from "@jest/globals";
 
-import { DEFAULT_NETWORK, CONFIG, chainToPlatform, chains } from "@wormhole-foundation/connect-sdk";
+import { DEFAULT_NETWORK, CONFIG, chainToPlatform, chains } from "@wormhole-foundation/sdk-connect";
 
-import { SuiChains, SuiPlatform } from './../../src/index.js';
+import { SuiChains, SuiPlatform } from "./../../src/index.js";
 
-import "@wormhole-foundation/connect-sdk-sui-core";
-import "@wormhole-foundation/connect-sdk-sui-tokenbridge";
+import "@wormhole-foundation/sdk-sui-core";
+import "@wormhole-foundation/sdk-sui-tokenbridge";
 import { SuiClient } from "@mysten/sui.js/client";
 
 const network = DEFAULT_NETWORK;
 
-const SUI_CHAINS = chains.filter((c) => chainToPlatform(c) === SuiPlatform._platform) as string[];
+const SUI_CHAINS = chains.filter(
+  (c) => chainToPlatform(c) === SuiPlatform._platform,
+) as SuiChains[];
+const suiChain = SUI_CHAINS[0]!;
 const configs = CONFIG[network].chains;
 
 describe("Sui Platform Tests", () => {
   describe("Get Token Bridge", () => {
     test("Hardcoded Genesis mock", async () => {
       const p = new SuiPlatform(network, {
-        [SUI_CHAINS[0]]: configs[SUI_CHAINS[0]],
+        [suiChain]: configs[suiChain],
       });
 
-      const client = new SuiClient({ url: configs[SUI_CHAINS[0]].rpc });
+      const client = new SuiClient({ url: configs[suiChain]!.rpc });
       const tb = await p.getProtocol("TokenBridge", client);
       expect(tb).toBeTruthy();
     });
@@ -30,14 +33,14 @@ describe("Sui Platform Tests", () => {
     test("No conf", () => {
       const p = new SuiPlatform(network, {});
       expect(p.config).toEqual({});
-      expect(() => p.getChain(SUI_CHAINS[0])).toThrow();
+      expect(() => p.getChain(suiChain)).toThrow();
     });
 
     test("With conf", () => {
       const p = new SuiPlatform(network, {
-        [SUI_CHAINS[0]]: configs[SUI_CHAINS[0]],
+        [suiChain]: configs[suiChain],
       });
-      expect(() => p.getChain(SUI_CHAINS[0])).not.toThrow();
+      expect(() => p.getChain(suiChain)).not.toThrow();
     });
   });
 
@@ -48,16 +51,16 @@ describe("Sui Platform Tests", () => {
 
       // expect getRpc to throw an error since we havent provided
       // the conf to figure out how to connect
-      expect(() => p.getRpc(SUI_CHAINS[0])).toThrow();
-      expect(() => p.getChain(SUI_CHAINS[0])).toThrow();
+      expect(() => p.getRpc(suiChain)).toThrow();
+      expect(() => p.getChain(suiChain)).toThrow();
     });
 
     test("With conf", () => {
       const p = new SuiPlatform(network, {
-        [SUI_CHAINS[0]]: configs[SUI_CHAINS[0]],
+        [suiChain]: configs[suiChain],
       });
-      expect(() => p.getRpc(SUI_CHAINS[0])).not.toThrow();
-      expect(() => p.getChain(SUI_CHAINS[0]).getRpc()).not.toThrow();
+      expect(() => p.getRpc(suiChain)).not.toThrow();
+      expect(() => p.getChain(suiChain).getRpc()).not.toThrow();
     });
   });
 });
