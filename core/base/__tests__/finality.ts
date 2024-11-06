3ff3ee408531bfd657c8ff83b8ca278dfcf102fd
Kevin Peters <kevin@wormholelabs.xyz>
2024-11-06 15:37:34 -0600

    fix tests

--- core/base/__tests__/finality.ts
+++ core/base/__tests__/finality.ts
@@ -7,7 +7,7 @@ import {
 
 describe("Finality tests", function () {
   test("Receive expected number of rounds", () => {
-    expect(finalityThreshold("Ethereum")).toEqual(96);
+    expect(finalityThreshold("Ethereum")).toEqual(72);
     expect(finalityThreshold("Algorand")).toEqual(0);
     expect(finalityThreshold("Solana")).toEqual(32);
   });
@@ -41,7 +41,7 @@ describe("Finality tests", function () {
   test("Estimates rounds from finalized consistency level", () => {
     // 100 + (# final rounds)
     expect(consistencyLevelToBlock("Ethereum", ConsistencyLevels.Finalized, fromBlock)).toEqual(
-      fromBlock + 96n,
+      fromBlock + 72n,
     );
     expect(consistencyLevelToBlock("Solana", ConsistencyLevels.Finalized, fromBlock)).toEqual(
       fromBlock + 32n,
