2783bd5e175df28d87b5c15a7cadbcb5da7c4bf5
Kevin Peters <kevin@wormholelabs.xyz>
2025-01-02 14:08:17 -0600

    aptos: migrate sdk to latest version
    use @aptos-labs/ts-sdk instead of deprecated aptos package

--- platforms/aptos/src/unsignedTransaction.ts
+++ platforms/aptos/src/unsignedTransaction.ts
@@ -1,12 +1,12 @@
 import type { Network, UnsignedTransaction } from "@wormhole-foundation/sdk-connect";
-import type { Types } from "aptos";
+import type { InputGenerateTransactionPayloadData } from "@aptos-labs/ts-sdk";
 import type { AptosChains } from "./types.js";
 
 export class AptosUnsignedTransaction<N extends Network, C extends AptosChains>
   implements UnsignedTransaction<N, C>
 {
   constructor(
-    readonly transaction: Types.EntryFunctionPayload,
+    readonly transaction: InputGenerateTransactionPayloadData,
     readonly network: N,
     readonly chain: C,
     readonly description: string,
