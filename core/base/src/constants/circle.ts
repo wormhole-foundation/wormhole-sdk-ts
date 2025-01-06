32418515db267ff387fbfa10e72c72b14ac6634c
Kevin Peters <kevin@wormholelabs.xyz>
2025-01-06 10:48:00 -0600

    skeleton code

--- core/base/src/constants/circle.ts
+++ core/base/src/constants/circle.ts
@@ -20,6 +20,8 @@ const usdcContracts = [[
     ["Base",      "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913"],
     ["Polygon",   "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359"],
     ["Sui",       "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC"],
+    // TODO: mainnet
+    //["Aptos",     ""]
   ]], [
   "Testnet", [
     ["Sepolia",         "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"],
@@ -30,6 +32,7 @@ const usdcContracts = [[
     ["BaseSepolia",     "0x036CbD53842c5426634e7929541eC2318f3dCF7e"],
     ["Polygon",         "0x9999f7fea5938fd3b1e26a12c3f2fb024e194f97"],
     ["Sui",             "0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC"],
+    ["Aptos",           "0x69091fbab5f7d635ee7ac5098cf0c1efbe31d68fec0f2cd565e8d168daf52832"]
   ]],
 ] as const satisfies MapLevel<Network, MapLevel<Chain, string>>;
 export const usdcContract = constMap(usdcContracts);
@@ -46,6 +49,7 @@ const circleDomains = [[
     ["Base",      6],
     ["Polygon",   7],
     ["Sui",       8],
+    ["Aptos",     9],
   ]], [
   "Testnet", [
     ["Sepolia",         0],
@@ -56,6 +60,7 @@ const circleDomains = [[
     ["BaseSepolia",     6],
     ["Polygon",         7],
     ["Sui",             8],
+    ["Aptos",           9],
   ]],
 ] as const satisfies MapLevel<Network, MapLevel<Chain, number>>;
 
