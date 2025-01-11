06240a2e61ae7dd0926c94cabb5f6e1ef6956c05
hernandp <hernan.di.pietro@gmail.com>
2025-01-11 02:56:15 -0300

    seievm SR testnet address.

--- core/base/src/constants/contracts/relayer.ts
+++ core/base/src/constants/contracts/relayer.ts
@@ -30,6 +30,7 @@ export const relayerContracts = [[
     ["Avalanche",       "0xA3cF45939bD6260bcFe3D66bc73d60f19e49a8BB"],
     ["Fantom",          "0x9563a59c15842a6f322b10f69d1dd88b41f2e97b"],
     ["Celo",            "0x306B68267Deb7c5DfCDa3619E22E9Ca39C374f84"],
+    ["Seievm",          "0x362fca37E45fe1096b42021b543f462D49a5C8df"],
     ["Moonbeam",        "0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0"],
     ["Arbitrum",        "0xAd753479354283eEE1b86c9470c84D42f229FF43"],
     ["Optimism",        "0x01A957A525a5b7A72808bA9D10c389674E459891"],
