50b9c919b81df30a89741948f876b7f3bbae5d5f
Artur Sapek <art@wormholelabs.xyz>
2025-01-13 11:05:04 -0500

    fix non-checksummed addresses

--- core/base/src/constants/circle.ts
+++ core/base/src/constants/circle.ts
@@ -12,13 +12,13 @@ export const circleAPI = constMap(circleAPIs);
 // prettier-ignore
 const usdcContracts = [[
   "Mainnet", [
-    ["Ethereum",  "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],
-    ["Avalanche", "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e"],
+    ["Ethereum",  "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"],
+    ["Avalanche", "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E"],
     ["Optimism",  "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85"],
     ["Arbitrum",  "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"],
     ["Solana",    "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"],
-    ["Base",      "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913"],
-    ["Polygon",   "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359"],
+    ["Base",      "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"],
+    ["Polygon",   "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"],
     ["Sui",       "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC"],
   ]], [
   "Testnet", [
@@ -28,7 +28,7 @@ const usdcContracts = [[
     ["ArbitrumSepolia", "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d"],
     ["Solana",          "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU"],
     ["BaseSepolia",     "0x036CbD53842c5426634e7929541eC2318f3dCF7e"],
-    ["Polygon",         "0x9999f7fea5938fd3b1e26a12c3f2fb024e194f97"],
+    ["Polygon",         "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97"],
     ["Sui",             "0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC"],
   ]],
 ] as const satisfies MapLevel<Network, MapLevel<Chain, string>>;
