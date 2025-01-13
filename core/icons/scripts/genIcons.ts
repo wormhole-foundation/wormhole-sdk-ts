8bfd2d38403a7d81d2b93b7888d15d23fc8f5b5c
Bruce Riley <bruce@wormholelabs.xyz>
2025-01-13 16:05:54 -0600

    Add Monad support

--- core/icons/scripts/genIcons.ts
+++ core/icons/scripts/genIcons.ts
@@ -21,7 +21,9 @@ const makeChainCondition = (chain: Chain): string => {
     return `(chain === "${chain}" || chain === "${chain}Sepolia")`;
   } else if (chain === "Sei") {
     return `(chain === "${chain}" || chain === "${chain}evm")`;
-  } else if (chain.includes("Sepolia") || chain.includes("Holesky") || chain === "Seievm") {
+  } else if (chain === "Monad") {
+    return `(chain === "${chain}" || chain === "${chain}Devnet")`;    
+  } else if (chain.includes("Sepolia") || chain.includes("Holesky") || chain === "Seievm" || chain === "MonadDevnet") {
     return "";
   } else {
     return `(chain === "${chain}")`;
