bda968270c0d7a7c11c6c7e3675e5c9b266c2f53
Artur Sapek <art@wormholelabs.xyz>
2025-04-11 12:01:38 -0400

    use Date.now

--- core/base/src/utils/time.ts
+++ core/base/src/utils/time.ts
@@ -1,6 +1,3 @@
 export const expiration = (hours: number = 0, minutes: number = 0, seconds: number = 0): Date => {
-  return (new Date(
-    new Date().valueOf() +
-    (((hours*60*60) + (minutes*60) + seconds) * 1000)
-  ));
+  return (new Date(Date.now() + (((hours*60*60) + (minutes*60) + seconds) * 1000)));
 }
