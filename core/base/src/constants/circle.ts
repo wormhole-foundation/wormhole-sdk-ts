1ffba30d58ff52d686f7930db8c37b3659d1a0ea
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2025-05-28 10:08:57 -0500

    added sonic and linea usdc (#849)

--- core/base/src/constants/circle.ts
+++ core/base/src/constants/circle.ts
@@ -21,7 +21,9 @@ const usdcContracts = [[
     ["Polygon",   "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"],
     ["Sui",       "0xdba34672e30cb065b1f93e3ab55318768fd6fef66c15942c9f7cb846e2f900e7::usdc::USDC"],
     ["Aptos",     "0xbae207659db88bea0cbead6da0ed00aac12edcdda169e591cd41c94180b46f3b"],
-    ["Unichain",  "0x078D782b760474a361dDA0AF3839290b0EF57AD6"]
+    ["Unichain",  "0x078D782b760474a361dDA0AF3839290b0EF57AD6"],
+    ["Sonic",     "0x29219dd400f2Bf60E5a23d13Be72B486D4038894"],
+    ["Linea",     "0x176211869cA2b568f2A7D4EE941E073a821EE1ff"]
   ]], [
   "Testnet", [
     ["Sepolia",         "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"],
@@ -33,13 +35,16 @@ const usdcContracts = [[
     ["Polygon",         "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97"],
     ["Sui",             "0xa1ec7fc00a6f40db9693ad1415d0c193ad3906494428cf252621037bd7117e29::usdc::USDC"],
     ["Aptos",           "0x69091fbab5f7d635ee7ac5098cf0c1efbe31d68fec0f2cd565e8d168daf52832"],
-    ["Unichain",        "0x31d0220469e10c4E71834a79b1f276d740d3768F"]
+    ["Unichain",        "0x31d0220469e10c4E71834a79b1f276d740d3768F"],
+    ["Sonic",           "0xA4879Fed32Ecbef99399e5cbC247E533421C4eC6"],
+    ["Linea",           "0xFEce4462D57bD51A6A552365A011b95f0E16d9B7"]
   ]],
 ] as const satisfies MapLevel<Network, MapLevel<Chain, string>>;
 export const usdcContract = constMap(usdcContracts);
 
 // prettier-ignore
 // https://developers.circle.com/stablecoins/supported-domains
+// CCTP V1 domains
 const circleDomains = [[
   "Mainnet", [
     ["Ethereum",  0],
