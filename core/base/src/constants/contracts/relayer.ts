6dcd6ea6cb8aaeb0278e12c224de055723849dd3
Hernán Di Pietro <hernan.di.pietro@gmail.com>
2025-07-29 17:03:00 -0300

    mezo + plume sr mainnet addresses (#883)

--- core/base/src/constants/contracts/relayer.ts
+++ core/base/src/constants/contracts/relayer.ts
@@ -24,6 +24,8 @@ export const relayerContracts = [[
     ["Ink",       "0x27428DD2d3DD32A4D7f7C497eAaa23130d894911"],
     ["Worldchain","0x1520cc9e779c56dab5866bebfb885c86840c33d3"],
     ["Unichain",  "0x27428DD2d3DD32A4D7f7C497eAaa23130d894911"],
+    ["Mezo",      "0x27428DD2d3DD32A4D7f7C497eAaa23130d894911"],
+    ["Plume",     "0x27428DD2d3DD32A4D7f7C497eAaa23130d894911"],
   ]], [
   "Testnet", [
     ["Ethereum",        "0x28D8F1Be96f97C1387e94A53e00eCcFb4E75175a"],
