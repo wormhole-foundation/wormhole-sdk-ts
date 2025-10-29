4faa8215fab7d8a713ff569a4468d6d00b818057
kev1n-peters <96065607+kev1n-peters@users.noreply.github.com>
2025-10-29 13:29:14 -0500

    feat: Fogo mainnet support (#941)

--- core/base/src/constants/nativeChainIds.ts
+++ core/base/src/constants/nativeChainIds.ts
@@ -57,6 +57,7 @@ const chainNetworkNativeChainIdEntries = [
       ["Stacks",    1n],
       ["CreditCoin",   102030n],
       ["Monad",     143n],
+      ["Fogo",      "CDLtwKnaCoK157uaHQDj4fHu72AyD2519Cphmpiq6hvT"], // genesis hash
     ],
   ],
   [
@@ -133,16 +134,18 @@ const chainNetworkNativeChainIdEntries = [
 export const networkChainToNativeChainId = constMap(chainNetworkNativeChainIdEntries);
 
 type NetworkChainToNativeChainId = ToMapping<typeof chainNetworkNativeChainIdEntries>;
-export type PlatformToNativeChainIds<P extends Platform, N extends Network = Network> =
-  PlatformToChains<P> extends infer C
-    ? C extends keyof NetworkChainToNativeChainId[N]
-      ? NetworkChainToNativeChainId[N][C]
-      : // If the platform doesn't support the network, return the widened type for
-        // mainnet
-        C extends keyof NetworkChainToNativeChainId["Mainnet"]
-        ? Widen<NetworkChainToNativeChainId["Mainnet"][C]>
-        : never
-    : never;
+export type PlatformToNativeChainIds<
+  P extends Platform,
+  N extends Network = Network,
+> = PlatformToChains<P> extends infer C
+  ? C extends keyof NetworkChainToNativeChainId[N]
+    ? NetworkChainToNativeChainId[N][C]
+    : // If the platform doesn't support the network, return the widened type for
+    // mainnet
+    C extends keyof NetworkChainToNativeChainId["Mainnet"]
+    ? Widen<NetworkChainToNativeChainId["Mainnet"][C]>
+    : never
+  : never;
 
 //When mapping a Platform and native chain id to a network and chain pair, we assume that the
 //  mapping is injective, i.e. that for any given platform there is only a single chain that
