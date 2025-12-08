b1431e7e3c4e16cf891c13c18b673ee3cd258a08
Douglas Galico <douglas.galico@gmail.com>
2025-12-08 15:46:02 +0100

    feat: sui tbr v4 executor route and consts

--- platforms/sui/protocols/tokenBridge/src/index.ts
+++ platforms/sui/protocols/tokenBridge/src/index.ts
@@ -2,10 +2,13 @@ import { _platform } from "@wormhole-foundation/sdk-sui";
 import { registerProtocol } from "@wormhole-foundation/sdk-connect";
 import { SuiTokenBridge } from "./tokenBridge.js";
 import { SuiAutomaticTokenBridge } from "./automaticTokenBridge.js";
+import { SuiExecutorTokenBridge } from "./executorTokenBridge.js";
 
 registerProtocol("Sui", "TokenBridge", SuiTokenBridge);
 registerProtocol("Sui", "AutomaticTokenBridge", SuiAutomaticTokenBridge);
+registerProtocol("Sui", "ExecutorTokenBridge", SuiExecutorTokenBridge);
 
 export * from "./tokenBridge.js";
 export * from "./automaticTokenBridge.js";
+export * from "./executorTokenBridge.js";
 export * from "./utils.js";
