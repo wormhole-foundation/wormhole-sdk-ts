9a68ed0a117297d26020c3ea8a301627b3abcdbe
connorstake <blindzeit@gmail.com>
2026-06-11 16:49:56 +0200

    feat: add Arc testnet USDC contract for CCTP

--- core/base/src/constants/circle.ts
+++ core/base/src/constants/circle.ts
@@ -47,6 +47,7 @@ const usdcContracts = [[
     ["Linea",           "0xFEce4462D57bD51A6A552365A011b95f0E16d9B7"],
     ["Seievm",          "0x4fCF1784B31630811181f670Aea7A7bEF803eaED"],
     ["HyperEVM",        "0x2B3370eE501B4a559b57D449569354196457D8Ab"],
+    ["Arc",             "0x3600000000000000000000000000000000000000"],
     // ["HyperCore",       "not-implemented"]
   ]],
 ] as const satisfies MapLevel<Network, MapLevel<Chain, string>>;
